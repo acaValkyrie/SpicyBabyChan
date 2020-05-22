4b15428a5ee7469f22ab705151de3099bb664528
Yadokame <yadokame3.14@gmail.com>
2020-05-22 15:25:43 +0900

    何とかごり押しで音声認識つけたけど、これどうやってほかのページ開いたときとかにも音声認識させんの？あとやっぱ音出るわ。

--- kadai/lib/routes/test_route.dart
+++ kadai/lib/routes/test_route.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'globals.dart' as globals;
 import 'SpeechRecognision.dart';
+import 'package:flutter_speech/flutter_speech.dart';
+import 'package:flutter/foundation.dart';
 
 class Test extends StatefulWidget {
   @override
@@ -9,15 +11,51 @@ class Test extends StatefulWidget {
 
 class TestState extends State<Test> {
 
+  SpeechRecognition speech;
+
+  bool speechRecognitionAvailable = false;
+  bool isListening = false;
+
+  String transcription = '';
+
+  //String _currentLocale = 'en_US';
+  Language selectedLang = languages.first;
+
+  @override
+  initState() {
+    super.initState();
+    activateSpeechRecognizer();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  void activateSpeechRecognizer() {
+    print('_MyAppState.activateSpeechRecognizer... ');
+    speech = new SpeechRecognition();
+    speech.setAvailabilityHandler(onSpeechAvailability);
+    speech.setRecognitionStartedHandler(onRecognitionStarted);
+    speech.setRecognitionResultHandler(onRecognitionResult);
+    speech.setRecognitionCompleteHandler(onRecognitionComplete);
+    speech.setErrorHandler(errorHandler);
+    speech.activate('jp_JP').then((res) {
+      setState(() => speechRecognitionAvailable = res);
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
 
     setState((){});
-
+/*
     String inputtext;
     bool isListening;
     inputtext = txtupdate(inputtext);
     isListening = islsn(isListening);
+*/
+    if(!isListening) {
+      start();
+    }
     
     if (isListening) {
       return Scaffold(
@@ -39,7 +77,7 @@ class TestState extends State<Test> {
           padding: EdgeInsets.all(10.0),
           margin: EdgeInsets.only(top:50.0,left:20.0,right:20.0),
           child: Text(
-            inputtext,
+            transcription,
             style: TextStyle(
               color: Colors.black,
               fontSize: 25.0,
@@ -52,6 +90,7 @@ class TestState extends State<Test> {
     }
 
     else return Scaffold(
+
       appBar:AppBar(title: Text("開発用ページ"),),
 
       floatingActionButton: FloatingActionButton(
@@ -70,7 +109,7 @@ class TestState extends State<Test> {
         padding: EdgeInsets.all(10.0),
         margin: EdgeInsets.only(top:50.0,left:20.0,right:20.0),
         child: Text(
-          inputtext,
+          transcription,
           style: TextStyle(
             color: Colors.black,
             fontSize: 25.0,
@@ -81,4 +120,46 @@ class TestState extends State<Test> {
       ),
     );
   }
+
+  //SpeechRecognisionFunction
+  void start() => speech.activate(selectedLang.code).then((_) {
+    return speech.listen().then((result) {
+      print('_MyAppState.start => result $result');
+      setState(() {
+        isListening = result;
+      });
+    });
+  });
+
+  void cancel() =>
+      speech.cancel().then((_) => setState(() => isListening = false));
+
+  void stop() => speech.stop().then((_) {
+    setState(() => isListening = false);
+  });
+
+  void onSpeechAvailability(bool result) =>
+      setState(() => speechRecognitionAvailable = result);
+
+  void onCurrentLocale(String locale) {
+    print('_MyAppState.onCurrentLocale... $locale');
+    setState(
+            () => selectedLang = languages.firstWhere((l) => l.code == locale));
+  }
+
+  void onRecognitionStarted() {
+    setState(() => isListening = true);
+  }
+
+  void onRecognitionResult(String text) {
+    print('_MyAppState.onRecognitionResult... $text');
+    setState(() => transcription = text);
+  }
+
+  void onRecognitionComplete(String text) {
+    print('_MyAppState.onRecognitionComplete... $text');
+    setState(() => isListening = false);
+  }
+
+  void errorHandler() => activateSpeechRecognizer();
 }
