545e5a0618cf1f6962c156ed77f219a8ad8c1a90
Yadokame <yadokame3.14@gmail.com>
2020-08-19 23:39:07 +0900

    コードの整形

--- kadai/lib/routes/test_route.dart
+++ kadai/lib/routes/test_route.dart
@@ -57,7 +57,7 @@ class TestState extends State<TestWidget> {
     setState(() {});
     print("exit setstate");
     globals.namedataG.contains(globals.inputText) ? isMach = true : isMach = false;
-    isMach ? Bcolor = Colors.red : Bcolor = Colors.black;
+    isMach ? Bcolor = Colors.red : Bcolor = Colors.black;//isMachの値によってボタンの色を変える
 
     print("isListening:");
     print(globals.isListening);
@@ -92,57 +92,57 @@ class TestState extends State<TestWidget> {
     if(isMach){
       //globals.callFunc();
     }
-      return Scaffold(
-          appBar: AppBar(title: Text("開発用ページ3"),),
+    return Scaffold(
+        appBar: AppBar(title: Text("開発用ページ3"),),
 
-          floatingActionButton: FloatingActionButton(
-            tooltip: 'Action!',
-            child: Icon(Icons.mic),
-            onPressed: () {},
-            backgroundColor: Bcolor,
-          ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Action!',
+          child: Icon(Icons.mic),
+          onPressed: () {},
+          backgroundColor: Bcolor,
+        ),
 
-          body: Column(
-              children: <Widget>[
-                Container(
-                  height: 200.0,
-                  width: double.infinity,
-                  decoration: new BoxDecoration(
-                      border: new Border.all(color: Colors.grey)
-                  ),
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
-                  child: Text(
-                    globals.inputText,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25.0,
-                      height: 1.0,
-                    ),
-                    maxLines: 10,
-                  ),
+        body: Column(
+            children: <Widget>[
+              Container(
+                height: 200.0,
+                width: double.infinity,
+                decoration: new BoxDecoration(
+                    border: new Border.all(color: Colors.grey)
                 ),
-                Container(
-                  height: 200.0,
-                  width: double.infinity,
-                  decoration: new BoxDecoration(
-                      border: new Border.all(color: Colors.grey)
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
+                child: Text(
+                  globals.inputText,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25.0,
+                    height: 1.0,
                   ),
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
-                  child: Text(
-                    globals.inputText2,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25.0,
-                      height: 1.0,
-                    ),
-                    maxLines: 10,
-                  ),
+                  maxLines: 10,
                 ),
-              ]
-          )
-      );
+              ),
+              Container(
+                height: 200.0,
+                width: double.infinity,
+                decoration: new BoxDecoration(
+                    border: new Border.all(color: Colors.grey)
+                ),
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
+                child: Text(
+                  globals.inputText2,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25.0,
+                    height: 1.0,
+                  ),
+                  maxLines: 10,
+                ),
+              ),
+            ]
+        )
+    );
   }
 
   List<CheckedPopupMenuItem<Language>> get _buildLanguagesWidgets => languages.map((l) => new CheckedPopupMenuItem<Language>(
